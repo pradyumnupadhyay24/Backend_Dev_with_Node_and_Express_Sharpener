fda81954311dd192843a6dd5c81ece68968c069f
Pradyumn24 <ayushpandit272204@gmail.com>
2023-09-06 04:38:55 +0530

    153. Getting a Single Product with the 'where' co… …ndition

--- module2/task17/src/controllers/shop.js
+++ module2/task17/src/controllers/shop.js
@@ -15,15 +15,34 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
-      res.render("shop/product-detail", {
-        product: product[0],
-        pageTitle: product.title,
-        path: "/products",
-      });
+  // Product.findById(prodId)
+  //   .then(product => {
+  //     res.render("shop/product-detail", {
+  //       product: product,
+  //       pageTitle: product.title,
+  //       path: "/products",
+  //     });
+  //   })
+    //.catch((err) => console.log(err));
+    // Using findAll with where clausure
+  // Product.findAll({where: {
+  //   id: prodId
+  // }}).then(products => {
+  //   res.render('shop/product-detail', { 
+  //     product: products[0], 
+  //     pageTitle: products[0].title, 
+  //     path: '/products'
+  //   })
+  // }).catch(err => console.log(err));
+  // Using find by pk
+  Product.findByPk(prodId)
+  .then((product) => {
+    res.render('shop/product-detail', { 
+      product: product, 
+      pageTitle: product.title, 
+      path: '/products'
     })
-    .catch((err) => console.log(err));
+  }).catch(err => console.log(err))
 };
 
 exports.getIndex = (req, res, next) => {
