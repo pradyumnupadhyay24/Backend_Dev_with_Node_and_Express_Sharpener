f801c1d5b52d8a6c0c652f564e2ba7be48ee4fa9
Pradyumn24 <ayushpandit272204@gmail.com>
2023-09-07 06:12:37 +0530

    Seperated CSS

--- module2/task18/src/app.js
+++ module2/task18/src/app.js
@@ -14,6 +14,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 const homeRoutes = require("./routes/homeroutes");
 app.use(homeRoutes);
 
+// with this users should be able to access 'public' path // it will take any request that tries to find some file.
+app.use(express.static(path.join(__dirname, "public")));
+
 app.listen(PORT , () =>{
     console.log(`Server Started at PORT ${PORT}`)
 });
